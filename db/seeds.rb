4dbef5497cdec7e251210912274a39bfcd07e2af
Devin Rieger <dev0089@gmail.com>
2017-03-03 02:28:09 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Song.create(title: "sup doodz", released: true, release_year: 1989, artist_name: "Lokja and the Boyz", genre: "HotS Punk")
+Song.create(title: "bruhhh", released: true, release_year: 2015, artist_name: "Dev0", genre: "Jam")
